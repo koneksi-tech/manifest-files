821b18983f9eab51ee8b11bef1a87b237c6d9156
Christian Longos <longoschristiiiaaan@gmail.com>
2026-04-09 23:38:26 +0800

    Remove deprecated MinIO configurations and related resources
    - Delete MinIO deployment, persistent volume claims, and associated Helm charts from NAS3.
    - Remove ingress configurations for MinIO services across various environments.
    - Clean up scripts and configuration files related to MinIO setup and object lock policies.
    - Ensure all references to MinIO resources are removed to streamline the deployment process.

--- minIO/NAS3/minio-notes.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-
-helm install minio-putulero --namespace minio-putulero -f minIO/NAS3/helm-minio-putulero.yml minio/minio
-helm install minio-skedulosa --namespace minio-skedulosa -f minIO/NAS3/helm-minio-skedulosa.yml minio/minio
-
-kubectl apply -f minIO/NAS3/ingress-minio-putulero.yml -n minio-putulero
-kubectl apply -f minIO/NAS3/ingress-minio-skedulosa.yml -n minio-skedulosa
--- mongodb/operator/README.md
+++
@@ -1,4 +0,0 @@
-# install operator with helm
-```
-helm install community-operator mongodb/community-operator --namespace mongodb-operator --create-namespace --set operator.watchNamespace="example"
-```
